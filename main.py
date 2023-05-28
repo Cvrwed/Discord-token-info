cbb7ad530d27587569d616b3d7b6c05aae8861f5
Crypt <qspoofing@gmail.com>
2023-05-28 10:51:00 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+from requests import get
+
+token = input("Ingresa el token del usuario > ")
+user = get("https://discord.com/api/users/@me", headers={'Authorization': token})
+
+if user.status_code == 401:
+    print("Token Inválido")
+    exit()
+
+rguilds = get("https://discord.com/api/users/@me/guilds", headers={'Authorization': token})
+rfriends = get("https://discord.com/api/v10/users/@me/relationships", headers={'Authorization': token})
+Data = user.json()
+guilds = rguilds.json()
+friends = rfriends.json()
+
+lang_mappings = {
+    'es': 'Spanish',
+    'en': 'English',
+    'da': 'Dansk',
+    'de': 'Deutsch',
+    'fr': 'French',
+    'hr': 'Croatian',
+    'it': 'Italian',
+    'lt': 'Lithuanian',
+    'hu': 'Hungarian',
+    'nl': 'Nederlands',
+    'no': 'Norwegian',
+    'pl': 'Polish',
+    'br': 'Brazil',
+    'ro': 'Romanian',
+    'fi': 'Suomi',
+    'sv': 'Swedish',
+    'vi': 'Vietnamese',
+    'tr': 'Turkish',
+    'cs': 'Czech',
+    'el': 'Greek',
+    'bg': 'Bulgarian',
+    'ru': 'Russian',
+    'uk': 'Ukrainian',
+    'hi': 'Hindi',
+    'th': 'Thai',
+    'zh': 'Chinese',
+    'ja': 'Japanese',
+    'ko': 'Korean',
+}
+
+lang_prefix = Data['locale'].split('-')[0].lower()
+language = lang_mappings.get(lang_prefix, 'Unknown')
+
+user_info = """
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Usuario: {username}#{discriminator}
+Telefono: {phone}
+ID: {id}
+Correo: {email}
+2fA: {mfa_enabled}
+Lenguaje: {language}
+Servidores: {num_guilds}
+Amigos: {num_friends}
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+""".format(username=Data['username'], discriminator=Data['discriminator'],
+        phone=Data['phone'] or "False", id=Data['id'], email=Data['email'],
+        mfa_enabled=Data['mfa_enabled'], language=language,
+        num_guilds=len(guilds), num_friends=len([friend for friend in friends if friend['type'] == 1]))[1:]
+print(user_info)
