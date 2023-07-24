4c705bb3f26aa4ae53b57bfd67c23dcc9b2818b9
Crypt <qspoofing@gmail.com>
2023-07-23 20:25:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from requests import get
+import requests
 
+w = requests.get
 token = input("Ingresa el token del usuario > ")
-user = get("https://discord.com/api/users/@me", headers={'Authorization': token})
+user = w("https://discord.com/api/users/@me", headers={'Authorization': token})
+Data = user.json()
 
 if user.status_code == 401:
     print("Token Inválido")
     exit()
 
-rguilds = get("https://discord.com/api/users/@me/guilds", headers={'Authorization': token})
-rfriends = get("https://discord.com/api/v10/users/@me/relationships", headers={'Authorization': token})
-Data = user.json()
+rguilds = w("https://discord.com/api/users/@me/guilds", headers={'Authorization': token})
+rfriends = w("https://discord.com/api/v10/users/@me/relationships", headers={'Authorization': token})
+
 guilds = rguilds.json()
 friends = rfriends.json()
 
@@ -49,17 +51,22 @@ language = lang_mappings.get(lang_prefix, 'Unknown')
 
 user_info = """
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-Usuario: {username}#{discriminator}
+Usuario: {user}#{discriminator}
 Telefono: {phone}
 ID: {id}
 Correo: {email}
 2fA: {mfa_enabled}
+Verificado: {verified_enabled}
+Nsfw Allowed: {nsfw_enabled}
 Lenguaje: {language}
 Servidores: {num_guilds}
 Amigos: {num_friends}
+Bio: {bio}
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-""".format(username=Data['username'], discriminator=Data['discriminator'],
-        phone=Data['phone'] or "False", id=Data['id'], email=Data['email'],
+""".format(user=Data['username'], discriminator=Data['discriminator'],
+        phone=Data['phone'] or "False", id=Data['id'], email=Data['email'] or "False",
         mfa_enabled=Data['mfa_enabled'], language=language,
-        num_guilds=len(guilds), num_friends=len([friend for friend in friends if friend['type'] == 1]))[1:]
+        num_guilds=len(guilds), num_friends=len(friends), 
+        verified_enabled=Data['verified'] or "False", bio=Data['bio'] or "N/A",
+        nsfw_enabled=Data['nsfw_allowed'] or "False")[1:]
 print(user_info)
